d71a2c18a31ec787572bd4620cfb3447cd59a897
Oluwatobi10 <Oluwole.OA@outlook.com>
2020-05-02 02:16:02 +0100

    Latest change

--- Hello World!!/Hello World!!/Program.cs
+++ Hello World!!/Hello World!!/Program.cs
@@ -10,7 +10,13 @@ namespace Hello_World__
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello World!!!"); //This is my first C# console application 
+            // i use "//" for single line comments 
+            /* i use this for multiple line comments 
+            so i can write as much comments as i want i this format
+            so as to explain alot about the codes above, below or inline 
+                just like this one here */
+                // any questions about the comment section???, please, #ASK 
         }
     }
 }
